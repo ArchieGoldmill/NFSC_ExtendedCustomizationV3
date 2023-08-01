fa0cf1ff57b8990cd40d42c258d956583ae26bfc
ARCHIE <ARCHIE@ARCHIE-PC>
2023-08-01 22:48:49 +0200

    Autosculpt camera

--- ExtendedCustomization/MenuCameras.h
+++ ExtendedCustomization/MenuCameras.h
@@ -5,6 +5,8 @@
 #include "Config.h"
 #include "FrontEndRenderingCar.h"
 #include "SteerAngle.h"
+#include "AutosculptSelectablePart.h"
+#include "FrontEndRenderingCar.h"
 
 void __stdcall GetCameraScreenName(Slot slot, int maxChars, char* screenName)
 {
@@ -26,6 +28,27 @@ void __stdcall GetCameraScreenName(Slot slot, int maxChars, char* screenName)
 	strcpy_s(screenName, maxChars, part.Camera.c_str());
 }
 
+void __cdecl GetCameraScreenNameAS(char* dest, const char* camera, int count)
+{
+	SteerAngle.Reset();
+
+	if (strcmp(camera, "FeAutosculptFrontBumper.fng") == 0)
+	{
+		auto part = AutosculptSelectablePart::GetCurrent();
+		if (part)
+		{
+			auto partConfig = g_Config.GetPart(part->SlotId, FrontEndRenderingCar::GetCarId());
+			if (partConfig.Camera.size())
+			{
+				strcpy_s(dest, count, partConfig.Camera.c_str());
+				return;
+			}
+		}
+	}
+
+	strcpy_s(dest, count, camera);
+}
+
 void __declspec(naked) GetCameraScreenNameCave()
 {
 	__asm
@@ -43,4 +66,5 @@ void __declspec(naked) GetCameraScreenNameCave()
 void InitMenuCameras()
 {
 	injector::MakeJMP(0x00840073, GetCameraScreenNameCave);
+	injector::MakeCALL(0x0084EB3A, GetCameraScreenNameAS, true);
 }
