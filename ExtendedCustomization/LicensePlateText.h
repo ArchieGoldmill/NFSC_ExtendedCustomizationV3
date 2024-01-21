a6e16d063368f57b130b0651f0ce3992991324b6
ARCHIE <ARCHIE@ARCHIE-PC>
2024-01-21 17:56:35 +0100

    Updated plates

--- ExtendedCustomization/LicensePlateText.h
+++ ExtendedCustomization/LicensePlateText.h
@@ -24,11 +24,11 @@ public:
 		auto plate = this->carRenderInfo->pRideInfo->GetPart(Slot::LICENSE_PLATE);
 		if (plate)
 		{
+			Hash plateHash = plate->GetAppliedAttributeIParam(Hashes::TEXTURE_NAME, 0);
 			if (plate->IsAutosculpt())
 			{
-				Hash plateh = plate->GetAppliedAttributeIParam(Hashes::TEXTURE_NAME, 0);
 				int plateIndex = carRenderInfo->pRideInfo->Autosculpt.Regions[ZoneLicenseplate].GetInt(0);
-				Hash plateHash = StringHash1(std::to_string(plateIndex).c_str(), plateh);
+				plateHash = StringHash1(std::to_string(plateIndex).c_str(), plateHash);
 				this->carRenderInfo->GeneralReplacementTextures[(int)ReplacementTextureIndex::LicensePlate].Set(Hashes::LICENSEPLATE, plateHash);
 
 				int frameIndex = carRenderInfo->pRideInfo->Autosculpt.Regions[ZoneLicenseplate].GetInt(1);
@@ -43,6 +43,10 @@ public:
 					Hash zoneValue = FromIndex("LICENSEPLATE_TEXT%d", textIndex);
 					this->carRenderInfo->GeneralReplacementTextures[(int)ReplacementTextureIndex::LicensePlate0 + i].Set(zoneHash, zoneValue);
 				}
+			}
+			else
+			{
+				this->carRenderInfo->GeneralReplacementTextures[(int)ReplacementTextureIndex::LicensePlate].Set(Hashes::LICENSEPLATE, plateHash);
 			}
 		}
 	}
