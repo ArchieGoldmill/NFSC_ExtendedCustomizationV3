bcbc797c655eee974cee52c2fb7332a9625f7dd7
ARCHIE <ARCHIE@ARCHIE-PC>
2024-01-21 13:25:30 +0100

    Fix decals

--- ExtendedCustomization/Decals.h
+++ ExtendedCustomization/Decals.h
@@ -7,5 +7,7 @@ void InitDecals()
 	{
 		// Get decal texture from texture name rather then part name
 		injector::WriteMemory<unsigned short>(0x007D5C35, 0x4277);
+
+		Game::CarPartSlotMap[(int)Slot::DECAL_REAR_WINDOW_TEX0] = 0x68;
 	}
 }
